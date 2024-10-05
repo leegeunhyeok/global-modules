7379da4f61c17f984051b94eae35c0403a97ddfc
leegeunhyeok <dev.ghlee@gmail.com>
2024-10-05 14:37:03 +0900

    chore: update build script

--- packages/runtime/build/index.mjs
+++ packages/runtime/build/index.mjs
@@ -3,8 +3,34 @@ import esbuild from 'esbuild';
 
 const rootDir = path.resolve(import.meta.dirname, '../src');
 
-await esbuild.build({
+/**
+ * @type {import('esbuild').BuildOptions}
+ */
+const baseBuildOptions = {
   bundle: true,
   entryPoints: [path.join(rootDir, 'index.ts')],
-  outdir: path.resolve(import.meta.dirname, '../dist'),
-});
+};
+
+await Promise.all([
+  esbuild.build({
+    ...baseBuildOptions,
+    format: 'cjs',
+    outdir: path.resolve(import.meta.dirname, '../dist'),
+  }),
+  esbuild.build({
+    ...baseBuildOptions,
+    format: 'cjs',
+    outExtension: {
+      '.js': '.cjs',
+    },
+    outdir: path.resolve(import.meta.dirname, '../cjs'),
+  }),
+  esbuild.build({
+    ...baseBuildOptions,
+    format: 'esm',
+    outExtension: {
+      '.js': '.mjs',
+    },
+    outdir: path.resolve(import.meta.dirname, '../esm'),
+  }),
+]);
