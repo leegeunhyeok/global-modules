409bb751ad1fb7b0874e76c4447c80d591ba8078
leegeunhyeok <dev.ghlee@gmail.com>
2024-10-26 09:18:52 +0900

    fix: update module exports

---
+++ packages/esbuild-plugin/build/index.mjs
@@ -0,0 +1,38 @@
+import path from 'node:path';
+import esbuild from 'esbuild';
+
+const rootDir = path.resolve(import.meta.dirname, '../src');
+
+/**
+ * @type {import('esbuild').BuildOptions}
+ */
+const baseBuildOptions = {
+  bundle: true,
+  entryPoints: [path.join(rootDir, 'index.ts')],
+  platform: 'node',
+  packages: 'external',
+};
+
+await Promise.all([
+  esbuild.build({
+    ...baseBuildOptions,
+    format: 'cjs',
+    outdir: path.resolve(import.meta.dirname, '../dist'),
+  }),
+  esbuild.build({
+    ...baseBuildOptions,
+    format: 'cjs',
+    outExtension: {
+      '.js': '.cjs',
+    },
+    outdir: path.resolve(import.meta.dirname, '../cjs'),
+  }),
+  esbuild.build({
+    ...baseBuildOptions,
+    format: 'esm',
+    outExtension: {
+      '.js': '.mjs',
+    },
+    outdir: path.resolve(import.meta.dirname, '../esm'),
+  }),
+]);
