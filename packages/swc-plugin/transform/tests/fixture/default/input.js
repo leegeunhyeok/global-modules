8c8c39f4a593828f234e3e94efbc02efe010e5a8
leegeunhyeok <dev.ghlee@gmail.com>
2024-11-30 12:36:55 +0900

    add dummy test

---
+++ packages/swc-plugin/transform/tests/fixture/default/input.js
@@ -0,0 +1,21 @@
+import React, { useState, useCallback } from 'react';
+import { foo } from './foo';
+import { bar as bar2 } from './bar';
+import * as baz from './baz';
+import * as foo2 from './foo';
+
+if (__DEV__) {
+  require('./dev');
+}
+
+export default function () {
+  //
+}
+
+export const named = 1;
+export { foo, foo2 };
+export { baz, baz as baz2 };
+export * from './re-exp';
+export * as rx from './re-exp-2';
+export { rx2 } from './re-exp-3';
+export { rx3 as rx4 } from './re-exp-4';
