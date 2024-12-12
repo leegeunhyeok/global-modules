d9f2ad43ae11dcb58caff1ce5ca65eb5edf0fde3
leegeunhyeok <dev.ghlee@gmail.com>
2024-12-13 03:38:36 +0900

    update demo

--- packages/swc-plugin/demo/index.mjs
+++ packages/swc-plugin/demo/index.mjs
@@ -10,19 +10,69 @@ const code = await fs.promises.readFile(
   },
 );
 
-const result = await swc.transform(code, {
-  filename: 'test.js',
-  jsc: {
-    target: 'esnext',
-    experimental: {
-      plugins: [
-        [
-          globalModulePlugin,
-          { id: 1000, phase: Phase.Register, dependencies: {} },
-        ],
-      ],
+function registerPreset() {
+  return [
+    globalModulePlugin,
+    {
+      id: 1,
+      phase: Phase.Register,
     },
-  },
-});
+  ];
+}
 
-console.log(result.code);
+function runtimePreset() {
+  return [
+    globalModulePlugin,
+    {
+      id: 1,
+      phase: Phase.Runtime,
+      dependencies: {
+        react: 1000,
+        './foo': 1001,
+        './bar': 1002,
+        './baz': 1003,
+        './Component': 1004,
+        './cjs-1': 1005,
+        './cjs-2': 1006,
+        './cjs-3': 1007,
+        './esm': 1008,
+        './re-exp': 1009,
+        './re-exp-1': 1010,
+        './re-exp-2': 1011,
+        './re-exp-3': 1012,
+        './re-exp-4': 1013,
+        './re-exp-5': 1014,
+      },
+    },
+  ];
+}
+
+// Register
+console.log(
+  (
+    await swc.transform(code, {
+      filename: 'test.js',
+      jsc: {
+        target: 'esnext',
+        experimental: {
+          plugins: [registerPreset()],
+        },
+      },
+    })
+  ).code,
+);
+
+// Runtime
+console.log(
+  (
+    await swc.transform(code, {
+      filename: 'test.js',
+      jsc: {
+        target: 'esnext',
+        experimental: {
+          plugins: [runtimePreset()],
+        },
+      },
+    })
+  ).code,
+);
