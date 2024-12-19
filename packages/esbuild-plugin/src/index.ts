642b61f48640b09cac6183df24ec6c1b7e46a626
leegeunhyeok <dev.ghlee@gmail.com>
2024-12-20 03:04:21 +0900

    update deps

--- packages/esbuild-plugin/src/index.ts
+++ packages/esbuild-plugin/src/index.ts
@@ -1,4 +1,4 @@
 export { createPlugin } from './create-plugin';
 
 export type { DependencyManager } from './dependency-manager';
-export type { EsbuildMeta, Module } from 'esbuild-dependency-graph';
+export type { Module } from 'esbuild-dependency-graph';
