71c5048463b6c90118f16fbb47991aacd4f5ba5d
leegeunhyeok <dev.ghlee@gmail.com>
2024-12-27 17:00:40 +0900

    add dev release

---
+++ .scripts/dev-release.mjs
@@ -0,0 +1,14 @@
+import { $ } from 'zx';
+
+const date = new Date();
+const timestamp =
+  date.getFullYear().toString().padStart(4, '0') +
+  String(date.getMonth() + 1).padStart(2, '0') +
+  String(date.getDate()).padStart(2, '0') +
+  String(date.getHours()).padStart(2, '0') +
+  String(date.getMinutes()).padStart(2, '0') +
+  String(date.getSeconds()).padStart(2, '0');
+const version = `0.0.0-dev.${timestamp}`;
+
+await $`git switch -c release/${version}`;
+await $`yarn nx release ${version}`;
