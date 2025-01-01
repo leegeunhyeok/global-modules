3ac90049c1684b2cd93189151eda35c7bcbc8ef5
leegeunhyeok <dev.ghlee@gmail.com>
2025-01-01 14:27:34 +0900

    feat(swc-plugin): avoid unnecessary exports call

--- packages/swc-plugin/transform/tests/fixture/runtime/esm_imports/output.js
+++ packages/swc-plugin/transform/tests/fixture/runtime/esm_imports/output.js
@@ -3,6 +3,3 @@ var { default: React, useState, useCallback, useMemo: useMemoization } = global.
 var { default: Default } = global.__modules.require("mod-1");
 var { foo, bar, baz } = global.__modules.require("mod-2");
 var all = global.__modules.require("mod-3");
-__ctx.exports(function() {
-    return {};
-});
